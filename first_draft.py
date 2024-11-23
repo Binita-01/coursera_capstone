86943351f2d305d418dd4bb0226d5dc06d03718e
Binita K C <121566296+Binita-01@users.noreply.github.com>
2024-11-23 10:35:59 -0600

    Update first_draft.py
    data collection overview

--- first_draft.py
+++ first_draft.py
@@ -1,2 +1,34 @@
 print("Hello World!")
 print("This is a new file")
+
+# adding api spacex
+"https://api.spacexdata.com/v4/"
+"https://api.spacexdata.com/v4/capsules"
+"https://api.spacexdata.com/v4/cores"
+
+url = "https://api.spacexdata.com/v4/launches/past"
+response = requests.get(url)
+response.json()
+
+# wrangling data using an API
+data = pd.json_normalize(response.json()) 
+# json data to flat data form
+
+# webscrape falcon 9 launch records
+
+# raw data to clean data
+# wrnaglidng data
+  # function, targets, endpoint
+  # another endpoint to get specific datas
+# sampling
+  # how to filter or remove falcon 1 data to get falcon 9 data
+# nulling
+  # data that contains null value; inside the payload mask
+  # calculate mean of payload and replace null with mean
+
+  
+  
+
+
+
+
